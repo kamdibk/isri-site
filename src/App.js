de0c5e209fa87fb534045866a061caab678c337a
Tejas Raikwar <raikwar.tejas@gmail.com>
2024-04-10 18:45:09 +0530

    navbar
    not scroling in media

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-Header">
+        <Navbar />
+      </div>
+      <div className="main-body">
+        {/* <Hero /> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,440 @@
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+import Logo from "../../assets/ISRI-LOGO.jpg";
+import { MdPhone } from "react-icons/md";
+import {
+  AiOutlineMail,
+  AiFillSetting,
+  AiFillCloud,
+  AiOutlineMobile,
+  AiOutlineDesktop,
+  AiOutlineClose,
+  AiOutlinePlus,
+  AiOutlineMinus,
+} from "react-icons/ai";
+import { FaHandshake } from "react-icons/fa";
+import { BsCart4, BsLaptop, BsFillPeopleFill } from "react-icons/bs";
+import { IoEarthOutline } from "react-icons/io5";
+import { RiMotorbikeFill } from "react-icons/ri";
+import { GrGamepad } from "react-icons/gr";
+import { TbRobot } from "react-icons/tb";
+import { GiArtificialIntelligence, GiHamburgerMenu } from "react-icons/gi";
+
+const Navbar = () => {
+  const [dropdownNav, setDropdownNav] = useState(false);
+  const [toggleService, setToggleService] = useState(false);
+  const toggleDropdown = () => {
+    setDropdownNav(!dropdownNav);
+  };
+
+  return (
+    <div className="fixed-nav">
+      <nav className="navbar">
+        <div className="com-logo">
+          <img src={Logo} alt="ISRI Logo" className="isri-logo" />
+        </div>
+        <div className="nav-links">
+          <ul className="nav-links-items">
+            <li>
+              <a href="#">SERVICES</a>
+              <div className="dropdown">
+                <div className="dropdown-lvl-1">
+                  <div className="services-links">
+                    <div style={{ padding: "10px" }}>
+                      <br />
+                      <span className="services-heading">SERVICES</span>
+                      <br />
+                      <br />
+                      <hr />
+                    </div>
+                    {/* list items */}
+                    <ul className="services-links-items">
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <AiFillSetting
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Software Development
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <BsCart4 size={20} style={{ marginRight: "10px" }} />
+                          eCommerce Solutions
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <AiOutlineMobile
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Mobile Apps Development
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <AiFillCloud
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Cloud Solution
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <AiOutlineDesktop
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Website Development
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <BsLaptop size={20} style={{ marginRight: "10px" }} />
+                          CMS Solutions
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <FaHandshake
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          CRM Solutions
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <IoEarthOutline
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Web Design
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <BsFillPeopleFill
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Hire Developer
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <RiMotorbikeFill
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Ride Booking
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <GrGamepad
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Gaming Solution
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <GiArtificialIntelligence
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          AI/ML
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <TbRobot size={20} style={{ marginRight: "10px" }} />
+                          Robotic Process Automation
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="services-image"></div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <a href="#">TECHNOLOGIES</a>
+            </li>
+            <li>
+              <a href="#">INDUSTRIES</a>
+            </li>
+            <li>
+              <a href="#">ABOUT US</a>
+            </li>
+            <li>
+              <a href="#">PORTFOLIO</a>
+            </li>
+            <li>
+              <a href="#">CASE STUDY</a>
+            </li>
+            <li>
+              <a href="#">BLOCK</a>
+            </li>
+          </ul>
+        </div>
+        <div className="com-contacts">
+          <div className="contact" style={{ borderBottom: "1px solid black" }}>
+            <MdPhone size={27} style={{ marginRight: "10px" }} color={"red"} />
+            <span className="contact-number">IN : +91-90825 82243</span>
+          </div>
+          <div className="contact">
+            <AiOutlineMail
+              size={27}
+              style={{ marginRight: "10px" }}
+              color={"red"}
+            />
+            <span className="contact-email">info@isritechnologies.com</span>
+            <div className="contact hamIcon" onClick={toggleDropdown}>
+              {dropdownNav ? (
+                <AiOutlineClose size={25} />
+              ) : (
+                <GiHamburgerMenu size={25} />
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
+      {dropdownNav ? (
+        <div className="nav-links-dropdown">
+          <div className="dropdown-nav-links">
+            <ul className="dropdown-nav-links-items">
+              <li onClick={() => setToggleService(!toggleService)}>
+                <a href="#">
+                  SERVICES
+                  {toggleService ? (
+                    <AiOutlineMinus />
+                  ):(
+                    <AiOutlinePlus size={22} />
+                  ) }
+                </a>
+                {toggleService ?(
+                  <div className="services-dropdown">
+                    <ul className="services-dropdown-links-items">
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <AiFillSetting
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Software Development
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <BsCart4 size={20} style={{ marginRight: "10px" }} />
+                          eCommerce Solutions
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <AiOutlineMobile
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Mobile Apps Development
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <AiFillCloud
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Cloud Solution
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <AiOutlineDesktop
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Website Development
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <BsLaptop size={20} style={{ marginRight: "10px" }} />
+                          CMS Solutions
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <FaHandshake
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          CRM Solutions
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <IoEarthOutline
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Web Design
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <BsFillPeopleFill
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Hire Developer
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <RiMotorbikeFill
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Ride Booking
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <GrGamepad
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          Gaming Solution
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <GiArtificialIntelligence
+                            size={20}
+                            style={{ marginRight: "10px" }}
+                          />
+                          AI/ML
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          style={{ alignItems: "center", display: "flex" }}
+                        >
+                          <TbRobot size={20} style={{ marginRight: "10px" }} />
+                          Robotic Process Automation
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                ) : null}
+              </li>
+              <li>
+                <a href="#">TECHNOLOGIES</a>
+              </li>
+              <li>
+                <a href="#">INDUSTRIES</a>
+              </li>
+              <li>
+                <a href="#">ABOUT US</a>
+              </li>
+              <li>
+                <a href="#">PORTFOLIO</a>
+              </li>
+              <li>
+                <a href="#">CASE STUDY</a>
+              </li>
+              <li>
+                <a href="#">BLOCK</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Navbar;
