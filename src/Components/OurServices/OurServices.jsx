e2cf944e27a1920b4cba86cf8f07ae205259a3ca
Tejas Raikwar <raikwar.tejas@gmail.com>
2024-04-17 17:01:24 +0530

    some box added

--- src/Components/OurServices/OurServices.jsx
+++ src/Components/OurServices/OurServices.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './OurServices.css'
+import { BsShieldLock } from "react-icons/bs";
 const OurServices = () => {
   return (
     <div className='ourservices-main-container'>
         <center className='ourservices-main-center'>
-            <div className="ourservices-item"></div>
+            <div className="ourservices-item">
+                <div className='ourservices-icon'><BsShieldLock size={32} color='blue' style={{fontWeight:"bolder"}}/></div>
+                <div className='ourservices-heading'></div>
+                <div className='ourservices-text'></div>
+                <div className='ourservices-button'></div>
+            </div>
             <div className="ourservices-item"></div>
             <div className="ourservices-item"></div>
             <div className="ourservices-item"></div>
