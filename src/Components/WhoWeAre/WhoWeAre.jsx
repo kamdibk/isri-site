600831313e5560c56a7a20de4e027e38cf1ed59f
Tejas Raikwar <raikwar.tejas@gmail.com>
2024-04-18 18:43:49 +0530

    new component created

--- src/Components/WhoWeAre/WhoWeAre.jsx
+++ src/Components/WhoWeAre/WhoWeAre.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-import './WhoWeAre.css'
+import React from "react";
+import "./WhoWeAre.css";
 const WhoWeAre = () => {
   return (
-    <div className='whoweare-container'>Who we are</div>
-  )
-}
+    <div className="whoweare-container">
+      <div className="whoweare-container-left">
+        <h4 style={{ 
+            color: "blue", 
+            fontWeight: "600", 
+            margin: "2rem" 
+            }}>
+          WHO WE ARE
+        </h4>
+        <div
+          style={{
+            margin: "2rem",
+            fontSize: "2.5rem",
+            fontWeight: "600",
+            fontFamily: "arial",
+          }}
+        >
+          Highly Tailored IT Design,
+          <br />
+          Management & Support <br />
+          Services.
+        </div>
+        <div style={{ 
+            width: "90%", 
+            margin: "2rem",
 
-export default WhoWeAre 
+             }}>
+          Accelerate innovation with world-class tech teams We’ll match you to
+          an entire remote team of incredible freelance talent for all your
+          software development needs.
+        </div>
+      </div>
+      <div className="whoweare-container-right"></div>
+    </div>
+  );
+};
+
+export default WhoWeAre;
