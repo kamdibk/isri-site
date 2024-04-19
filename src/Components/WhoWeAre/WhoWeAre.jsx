de08daa983af609a1d748426195026a9252c9451
Tejas Raikwar <raikwar.tejas@gmail.com>
2024-04-19 13:00:09 +0530

    some changes

--- src/Components/WhoWeAre/WhoWeAre.jsx
+++ src/Components/WhoWeAre/WhoWeAre.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./WhoWeAre.css";
+import sign from '../../images/sign.png'
 const WhoWeAre = () => {
   return (
     <div className="whoweare-container">
@@ -26,12 +27,35 @@ const WhoWeAre = () => {
         </div>
         <div style={{ 
             width: "90%", 
-            margin: "2rem",
-
+            margin: "2rem 2rem 1.2rem 2rem",
+            lineHeight:"30px",
+            color:"rgb(76, 77, 86)",
+            fontFamily:"Jost"
              }}>
           Accelerate innovation with world-class tech teams We’ll match you to
           an entire remote team of incredible freelance talent for all your
           software development needs.
+        </div>
+        <div className="inner-div">
+            <div className="inner-div-child-left">
+              <div className="who-we-are-left-sign">
+                <img src={sign} alt="sig" />
+              </div>
+              <div className="who-we-are-left-name">
+                Lorem Bhau
+              </div>
+              <div className="who-we-are-left-desig">
+                CEO, Somewhere
+              </div>
+            </div>
+            <div className="inner-div-child-right">
+                <div className="who-we-are-text">
+                  Call to ask any question
+                </div>
+                <div className="who-we-are-num">
+                    +91 90825 82243
+                </div>
+            </div>
         </div>
       </div>
       <div className="whoweare-container-right"></div>
