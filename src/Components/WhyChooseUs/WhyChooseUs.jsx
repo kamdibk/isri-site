7e365152ccde5944fb4e93950cd8600bc87b5931
Tejas Raikwar <raikwar.tejas@gmail.com>
2024-04-23 17:57:52 +0530

    some major changes

--- src/Components/WhyChooseUs/WhyChooseUs.jsx
+++ src/Components/WhyChooseUs/WhyChooseUs.jsx
@@ -1,4 +1,6 @@
 import "./WhyChooseUs.css";
+import { WhyChooseUsData } from "../../Data/WhyChooseUsData";
+
 const WhyChooseUs = () => {
   return (
     <div className="whychooseus-container">
@@ -6,7 +8,22 @@ const WhyChooseUs = () => {
       <div className="whychooseus-subcontainer">
         <div className="whychooseus-content">
           <h4 className="heading-text">WHY CHOOSE US</h4>
-            <h1 className="h1-txt">WE TRANSFORM YOUR BUSINESS</h1>
+            <h1 className="
+            h1-txt stroke-text">WE TRANSFORM YOUR BUSINESS</h1>
+            <div className="whychooseus-rsn-blocks">
+                {/* <div className="whychooseus-rsn-block"></div> */}
+                {
+                  WhyChooseUsData.map((key)=>{
+                    return(
+                      <div className="whychooseus-rsn-block" key={key.id}>
+                        <div className="rsn-block-icon">{key.icon}</div>
+                        <div className="rsn-block-heading">{key.heading}</div>
+                        <div className="rsn-block-text">{key.description}</div>
+                      </div>
+                    )
+                  })
+                }
+            </div>
         </div>
       </div>
     </div>
--- src/Data/WhyChooseUsData.js
+++ src/Data/WhyChooseUsData.js
@@ -1,11 +1,41 @@
+import { MdInsertEmoticon } from "react-icons/md";
+const iconSize = 55;
+const iconColor = "rgb(7, 89, 127)"
 export const WhyChooseUsData = [
     {
-
+        id: 1,
+        icon: <MdInsertEmoticon size={iconSize} color={iconColor} className="rotate-icon"/>,
+        heading:"this is heading",
+        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt repellat aspernatur quia sapiente qui accusamus ipsum quos fugiat facere temporibus."
     },
     {
-
+        id: 2,
+        icon: <MdInsertEmoticon  size={iconSize} color={iconColor} className="rotate-icon"/>,
+        heading:"this is heading",
+        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt repellat aspernatur quia sapiente qui accusamus ipsum quos fugiat facere temporibus."
     },
     {
-        
-    }
+        id: 3,
+        icon: <MdInsertEmoticon  size={iconSize} color={iconColor} className="rotate-icon"/>,
+        heading:"this is heading",
+        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt repellat aspernatur quia sapiente qui accusamus ipsum quos fugiat facere temporibus."
+    },
+    {
+        id: 4,
+        icon: <MdInsertEmoticon  size={iconSize} color={iconColor} className="rotate-icon"/>,
+        heading:"this is heading",
+        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt repellat aspernatur quia sapiente qui accusamus ipsum quos fugiat facere temporibus."
+    },
+    {
+        id: 5,
+        icon: <MdInsertEmoticon  size={iconSize} color={iconColor} className="rotate-icon"/>,
+        heading:"this is heading",
+        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt repellat aspernatur quia sapiente qui accusamus ipsum quos fugiat facere ."
+    },
+    {
+        id: 6,
+        icon: <MdInsertEmoticon  size={iconSize} color={iconColor} className="rotate-icon"/>,
+        heading:"this is heading",
+        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt repellat aspernatur quia sapiente qui accusamus ipsum quos fugiat facere temporibus quos fugiat facere temporibus."
+    },
 ]
