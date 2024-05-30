70212243a0506e2a0bce1bd7eeb17ede60852719
Tejas Raikwar <raikwar.tejas@gmail.com>
2024-05-30 19:18:51 +0530

    -

---
+++ src/Components/ServicesComponent/ServicesComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "ServicesComponent.css"
+const ServicesComponent = () => {
+  return (
+    <div>
+      gfhdghdghgfhdfghdfg
+    </div>
+  )
+}
+
+export default ServicesComponent
